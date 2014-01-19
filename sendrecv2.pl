83b6a60bc10b0c1ea8912378dbb70ebad3c93344
Dylan Cali <calid1984@gmail.com>
2014-01-19 07:16:11 -0600

    code comments were incorrect

--- sendrecv2.pl
+++ sendrecv2.pl
@@ -9,7 +9,7 @@ use ZMQ::FFI::Constants qw(ZMQ_REQ ZMQ_REP);
 
 my $endpoint = "ipc://zmq-ffi-$$";
 
-# send request using 2.x context
+# send using 2.x context
 my $req_ctx     = ZMQ::FFI->new( soname => 'libzmq.so.1' );
 my $req_version = join '.', $req_ctx->version;
 
@@ -17,7 +17,7 @@ my $req = $req_ctx->socket(ZMQ_REQ);
 $req->connect($endpoint);
 $req->send($req_version);
 
-# receive reply using 4.x context
+# receive using 4.x context
 my $rep_ctx     = ZMQ::FFI->new( soname => 'libzmq.so.3' );
 my $rep_version = join '.', $rep_ctx->version;
 
